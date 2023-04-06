b0f88c23ac2023794aeb47a67b8c5acd04da5831
homexhome <homexhome@proton.me>
2023-04-06 16:52:05 +0300

    Code cleanup - removed unnececary using statements

--- Program.cs
+++ Program.cs
@@ -1,13 +1,11 @@
-﻿using System.Linq;
-
-namespace Module15
+﻿namespace Module15
 {
     class Program
     {
         static void Main(string[] args)
         {
             var classes = new[]
-            {           
+            {
                new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
                new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
                new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
